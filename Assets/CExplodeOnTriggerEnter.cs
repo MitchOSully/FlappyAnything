1265193048a5d30ffc26687475cc0a9aff0ee264
MitchOSully <mitchell.osullivan@live.com.au>
2023-09-17 17:43:46 +0800

    Implement rocket explosion
    - Explosion occurs at contact point between player and rocket
    - A force is applied to the player in the direction of the vector
      joining the player and the rocket.

---
+++ Assets/CExplodeOnTriggerEnter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CExplodeOnTriggerEnter : MonoBehaviour
+{
+    public GameObject m_explosionPrefab;
+    public float m_fForceScale = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            CreateExplosion(collider);
+            PushPlayer(collider.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void CreateExplosion(Collider2D collider)
+    {
+        Vector2 collisionPt = collider.ClosestPoint(transform.position);
+        Instantiate(m_explosionPrefab, collisionPt, Quaternion.identity);
+    }
+
+    private void PushPlayer(GameObject player)
+    {
+        Vector2 pushDir = player.transform.position - transform.position;
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        rb.AddForce(pushDir * m_fForceScale);
+    }
+}
