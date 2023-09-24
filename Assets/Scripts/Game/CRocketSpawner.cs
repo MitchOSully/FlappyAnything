35dcdffc5b2caf51d769b2de74665252d8e1fd18
MitchOSully <mitchell.osullivan@live.com.au>
2023-09-24 15:38:55 +0800

    Better initialisation of rocket spawn wait time
    - You can now specify the pipe number after which you want the first
      rocket to hit the player, then all the calculations are done behind
      the scenes to set the spawn wait time to make that happen.

--- Assets/Scripts/Game/CRocketSpawner.cs
+++ Assets/Scripts/Game/CRocketSpawner.cs
@@ -3,23 +3,20 @@ using UnityEngine;
 
 public class CRocketSpawner : CObjectSpawner
 {
-    public float m_fStartWaitTime = 0;
+    public int m_iFirstRocketPipe = 2; //First rocket will hit player after this pipe
+                                       //Note: doesn't work when =1
+    public float m_fRocketPipeOffset = 0.5f; //The fraction of the distance between two pipes to place the rocket
     public CAutoMove m_pipeMoveScript;
     public CPipeSpawner m_pipeSpawner;
     public GameObject m_pipePrefab;
 
     private bool m_bWaiting = true;
+    public float m_fStartWaitTime = 0; //Time to wait before spawning first rocket
 
     void Start()
     {
-        BoxCollider2D pipeBoxCollider = m_pipePrefab.GetComponentInChildren<BoxCollider2D>();
-        float fPipeWidth = (pipeBoxCollider != null) ? pipeBoxCollider.size.x : 0;
-        float fPipeSpeed = m_pipeMoveScript.m_fMoveSpeed;
-        float fPipeSpawnDeltaTime = m_pipeSpawner.m_fSpawnDeltaTime;
-        float fPipeGapLength = fPipeSpeed * fPipeSpawnDeltaTime - fPipeWidth;
-
-        m_fSpawnDeltaTime = (fPipeGapLength + fPipeWidth) / fPipeSpeed;
-        //m_fStartWaitTime = m_fSpawnDeltaTime - (this.transform.position.x - fPipeGapLength) / fRocketSpeed;
+        m_fSpawnDeltaTime = m_pipeSpawner.m_fSpawnDeltaTime;
+        m_fStartWaitTime = CalculateStartWaitTime();
     }
 
     protected override void Update()
@@ -33,5 +30,37 @@ public class CRocketSpawner : CObjectSpawner
         {
             base.Update();
         }
+    }
+
+    private float CalculateStartWaitTime()
+    {
+        float fStartWaitTime = 0; //Return value
+
+        CAutoMove pipeMoveScript   = m_pipePrefab.GetComponent<CAutoMove>();
+        CAutoMove rocketMoveScript = m_prefab.GetComponent<CAutoMove>();
+        if (pipeMoveScript && rocketMoveScript)
+        {
+            float fFirstPipeTime = CalculateFirstPipeTime(pipeMoveScript);
+
+            float fRocketSpeed = rocketMoveScript.m_fMoveSpeed;
+            float fRocketTravelTime = transform.position.x / fRocketSpeed;
+
+            float fOffsetTime = m_pipeSpawner.m_fSpawnDeltaTime * m_fRocketPipeOffset;
+
+            fStartWaitTime = fFirstPipeTime + m_fSpawnDeltaTime * (m_iFirstRocketPipe - 1) - fRocketTravelTime + fOffsetTime; ;
+        }
+
+        return fStartWaitTime;
+    }
+
+    //Time at which first pipe reaches player
+    private float CalculateFirstPipeTime(CAutoMove pipeMoveScript)
+    {
+        float fPipeSpeed = pipeMoveScript.m_fMoveSpeed;
+        float fPipeSpawnerPos = transform.position.x; //Assume pipe and rocket spawner at same location
+        float fFirstPipeSpawnPos = fPipeSpawnerPos - fPipeSpeed * m_pipeSpawner.m_fSpawnDeltaTime * 2;
+        float fFirstPipeTime = fFirstPipeSpawnPos / fPipeSpeed;
+
+        return fFirstPipeTime;
     }
 }
